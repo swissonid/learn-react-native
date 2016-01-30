cff0f5bde4aba514f9f91af13eda3be1898a90b4
Patrice Mueller <mueller.patrice@gmail.com>
2016-01-30 18:32:54 +0100

    Added Input fields

--- App/Components/NoteScreen.js
+++ App/Components/NoteScreen.js
@@ -1,23 +1,44 @@
 import React, {
-  StyleSheet,
-  Text,
-  View
+    StyleSheet,
+    Text,
+    TextInput,
+    View
 } from 'react-native';
 
 export default class NoteScreen extends React.Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text>Create Note Screen!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    placeholder="Untitled"
+                    autoFocus={true}
+                    onEndEditing={() => {this.refs.body.focus()}}
+                    style={styles.title}
+                />
+                <TextInput
+                    ref="body"
+                    placeholder="Start typing"
+                    multilines={true}
+                    style={styles.body}
+                />
+            </View>
+        );
+    }
 }
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingTop: 64,
+        marginRight: 16,
+        marginLeft: 16
+    },
+    title: {
+        height: 40
+    },
+    body: {
+        flex: 1
+    }
 });
--- index.android.js
+++ index.android.js
@@ -4,49 +4,97 @@
  */
 'use strict';
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View,
-  Navigator
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    Navigator
 } from 'react-native';
 
 import NoteScreen from './App/Components/NoteScreen';
 import SimpleButton from './App/Components/SimpleButton';
-class ReactNotes extends Component {
-    
-    renderScene (route, navigator) {
+import HomeScreen from './App/Components/HomeScreen';
+
+var NavigationBarRouteMapper = {
+    LeftButton: function (route, navigator, index, navState) {
+        switch (route.name) {
+            case 'createNote':
+                return (
+                    <SimpleButton
+                        onPress={() => navigator.pop()}
+                        customText='Back'
+                    />
+                );
+            default:
+                return null;
+        }
+    },
+    RightButton: function (route, navigator, index, navState) {
         switch (route.name) {
             case 'home':
-               return (
-                   <View style={styles.container}>
-                    <SimpleButton onPress={() => navigator.push({name:'createNote'})}
-                        customText = 'Create Note'/>
-                   </View>
-               ); 
+                return (
+                    <SimpleButton
+                        onPress={() => navigator.push({name:'createNote'})}
+                        customText='Create Note'/>
+                );
+            default:
+                return null;
+        }
+    },
+    Title: function (route, navigator, index, navState) {
+        switch (route.name) {
+            case 'home':
+                return (
+                    <Text>React Notes</Text>
+                );
+            case 'createNote':
+                return (
+                    <Text>Create Note</Text>
+                );
+        }
+
+    }
+};
+
+
+class ReactNotes extends Component {
+
+    renderScene(route, navigator) {
+        switch (route.name) {
+            case 'home':
+                return (
+                    <HomeScreen />
+                );
             case 'createNote':
                 return (
                     <NoteScreen />
                 );
         }
     }
+
     render() {
         return (
             <Navigator
-            initialRoute={{name: 'home'}}
-            renderScene={this.renderScene}
+                initialRoute={{name: 'home'}}
+                renderScene={this.renderScene}
+                navigationBar={
+                    <Navigator.NavigationBar
+                        routeMapper={NavigationBarRouteMapper}
+                    />
+
+                }
             />
         );
     }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
 
 AppRegistry.registerComponent('ReactNotes', () => ReactNotes);
